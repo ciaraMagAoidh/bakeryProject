8a8836aa58a05d0d11e5b944c0f8ba033f6ced69
Ciara <cmagee1@live.co.uk>
2020-04-14 19:01:37 +0100

    updated bakery file code
    Signed-off-by: Ciara <cmagee1@live.co.uk>

--- bakery.py
+++ bakery.py
@@ -2,5 +2,7 @@ cake = input('do you want to order a cake? yes or no')
 
 if cake == 'yes':
     print('you want a cake')
-else:
+elif cake == 'no':
     print('you dont want a cake')
+else:
+    print(' invalid answer')
