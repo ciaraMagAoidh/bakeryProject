f316214b2e0270e8f0c89a8204a9dd0061be41e8
Lydia Quinn <lquinn584@qub.ac.uk>
2020-04-14 19:13:42 +0100

    Add maybe option to bakery.py

--- bakery.py
+++ bakery.py
@@ -4,5 +4,7 @@ if cake == 'yes':
     print('you want a cake')
 elif cake == 'no':
     print('you dont want a cake')
+elif cake == 'maybe':
+    print('awk go on')
 else:
     print(' invalid answer')
